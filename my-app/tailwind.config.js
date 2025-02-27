36aed7738bb97e6dbdc3943558036c93b5c8a27c
rylandbangerter <137819443+rylandbangerter@users.noreply.github.com>
2025-02-27 16:06:40 -0700

    react stuff

---
+++ my-app/tailwind.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  };
+
+
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
+}
